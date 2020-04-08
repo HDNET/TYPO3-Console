20ea88c784cff9361b6f3232c8430d02f205309d
Tim Spiekerkötter <tim.spiekerkoetter@hdnet.de>
2020-04-08 13:53:16 +0200

    Move warning from exception to error

--- Classes/Console/Core/Booting/Scripts.php
+++ Classes/Console/Core/Booting/Scripts.php
@@ -81,8 +81,8 @@ class Scripts
 
     public static function initializeErrorHandling()
     {
-        $enforcedExceptionalErrors = E_WARNING | E_USER_WARNING | E_USER_ERROR | E_RECOVERABLE_ERROR;
-        $errorHandlerErrors = $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] ?? E_ALL & ~(E_STRICT | E_NOTICE | E_COMPILE_WARNING | E_COMPILE_ERROR | E_CORE_WARNING | E_CORE_ERROR | E_PARSE | E_ERROR);
+        $enforcedExceptionalErrors = E_USER_WARNING | E_USER_ERROR | E_RECOVERABLE_ERROR;
+        $errorHandlerErrors = $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] ?? E_ALL & ~(E_WARNING | E_STRICT | E_NOTICE | E_COMPILE_WARNING | E_COMPILE_ERROR | E_CORE_WARNING | E_CORE_ERROR | E_PARSE | E_ERROR);
         // Ensure all exceptional errors are handled including E_USER_NOTICE
         $errorHandlerErrors = $errorHandlerErrors | E_USER_NOTICE | $enforcedExceptionalErrors;
         // Ensure notices are excluded to avoid overhead in the error handler
